2add5e2239f51196cd8a88f2cb9b18c32defb6e1
dhruv1897 <66731309+dhruv1897@users.noreply.github.com>
2024-04-25 18:58:38 -0700

    Changes

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 require("./models/registration");
 const app = require("./app");
+const fs = require('fs');
+const https = require('https');
 
 // Connecting to the MongoDB database
 mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// Certificate
+const privateKey = fs.readFileSync('serverp.pem', 'utf8'); // Update the path to match your directory structure
+const certificate = fs.readFileSync('serverp.pem', 'utf8'); // Update the path to match your directory structure
+const credentials = { key: privateKey, cert: certificate };
 
 // Handling MongoDB connection events
 mongoose.connection
@@ -19,8 +26,10 @@ mongoose.connection
     console.error(`Connection error: ${err.message}`);
   });
 
+// Starting the HTTPS server
+const httpsServer = https.createServer(credentials, app);
 
 // Starting the Express server
-const server = app.listen(3000, () => {
-  console.log(`Express is running on port ${server.address().port}`);
+httpsServer.listen(3000, () => {
+  console.log(`Express is running on port 3001`);
 });
